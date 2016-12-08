2f803fe75d1a72cb5e61f5920bea43e802fc3cf9
renan-garcia <email@renangarcia.me>
2016-12-08 15:19:21 -0200

    API configurada
    Criadas configurações do EF consumindo do Docker e primeiros metodos da API.

---
+++ src/WebAPIApplication/Models/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIApplication {
+    public class DataContext : DbContext{
+        public DataContext(DbContextOptions<DataContext>  options) : base(options)
+        {
+            
+        }
+        public DbSet<Pessoa> Pessoas{ get;set; }
+    }
+}
