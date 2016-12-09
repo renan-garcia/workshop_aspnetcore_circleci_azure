927754f65e10674cc806bcb4f5407e09cbe1cbf3
renan-garcia <email@renangarcia.me>
2016-12-09 00:23:08 -0200

    Configuração do CircleCI

--- src/WebAPIApplication/Controllers/PessoasController.cs
+++ src/WebAPIApplication/Controllers/PessoasController.cs
@@ -1,64 +1,75 @@
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
-namespace WebAPIApplication {
+namespace WebAPIApplication
+{
     [Route("api/pessoas")]
-    public class PessoasController : Controller {
+    public class PessoasController : Controller
+    {
         private readonly DataContext _dataContext;
 
-        public PessoasController(DataContext dataContext){
+        public PessoasController(DataContext dataContext)
+        {
             _dataContext = dataContext;
         }
 
         [HttpGet]
-        public async Task<IActionResult> ObterPessoas(){
+        public async Task<IActionResult> ObterPessoas()
+        {
             var pessoas = await _dataContext.Pessoas.ToListAsync();
             return Json(pessoas);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> ObterPessoa(int id){
-            var pessoa = await _dataContext.Pessoas.FindAsync(id);
-
-            if(pessoa == null)
-                return NotFound();
-            
-            return Json(pessoa);
-        }
-
         [HttpPost]
-        public async Task<IActionResult> CriaPessoa([FromBody]Pessoa modelo){
+        public async Task<IActionResult> CriaPessoa([FromBody]Pessoa modelo)
+        {
             await _dataContext.Pessoas.AddAsync(modelo);
             await _dataContext.SaveChangesAsync();
+
             return Json(modelo);
         }
-
-        [HttpPost]
-        public async Task<IActionResult> AtualizaPessoa([FromBody]Pessoa modelo){
-            var pessoa = await _dataContext.Pessoas.FindAsync(modelo.Id);
-
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObterPessoa(int id)        
+        {
+            var pessoa = await  _dataContext.Pessoas.FindAsync(id);    
+            
             if(pessoa == null)
                 return NotFound();
+
+            return Json(pessoa);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizaPessoa(int id, [FromBody]Pessoa modelo)        
+        {
+            var pessoa = await  _dataContext.Pessoas.FindAsync(id);    
             
+            if(pessoa == null)
+                return NotFound();
+
             pessoa.Nome = modelo.Nome;
             pessoa.Twitter = modelo.Twitter;
 
             await _dataContext.SaveChangesAsync();
-            return Json(modelo);
+
+            return Json(pessoa);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> RemovePessoa(int id){
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemovePessoa(int id)        
+        {
             var pessoa = await _dataContext.Pessoas.FindAsync(id);
 
             if(pessoa == null)
                 return NotFound();
-            
+
             _dataContext.Pessoas.Remove(pessoa);
-            
+
             await _dataContext.SaveChangesAsync();
+
             return StatusCode((int)HttpStatusCode.OK);
         }
     }
--- test/UnitTest/Controllers/PessoasControllerIntegrationTest.cs
+++ test/UnitTest/Controllers/PessoasControllerIntegrationTest.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using WebAPIApplication;
 using Xunit;
+using System.Net.Http;
+using System.Text;
+using System.Net;
 
 namespace UnitTest
 {
@@ -28,11 +31,12 @@ namespace UnitTest
         [Fact]
         public async Task DeveRetornarListaDePessoas()
         {
-            var pessoa =  new Pessoa
+            var pessoa = new Pessoa
             {
-                 Nome = "Renan Garcia",
-                 Twitter = "@_renan_garcia"
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
             };
+
             await TestDataContext.AddAsync(pessoa);
             await TestDataContext.SaveChangesAsync();
 
@@ -43,7 +47,147 @@ namespace UnitTest
             var data = JsonConvert.DeserializeObject<List<Pessoa>>(responseString);
 
             Assert.Equal(data.Count, 1);
-            Assert.Contains(data, p => p.Nome == pessoa.Nome);
+            Assert.Contains(data, x => x.Nome == pessoa.Nome);
+        }
+
+        [Fact]
+        public async Task DeveAdicionarPessoa()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            var response = await Client.PostAsync(BaseUrl, new StringContent(JsonConvert.SerializeObject(pessoa), Encoding.UTF8, "application/json"));            
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<Pessoa>(responseString);
+
+            Assert.Equal(data.Nome, pessoa.Nome);
+        }
+
+        [Fact]
+        public async Task DeveAtualizarPessoa()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            await TestDataContext.AddAsync(pessoa);
+            await TestDataContext.SaveChangesAsync();
+           
+           var pessoaEditada = new Pessoa
+           { 
+                Id = pessoa.Id,
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            var response = await Client.PutAsync($"{BaseUrl}/{pessoa.Id}", new StringContent(JsonConvert.SerializeObject(pessoaEditada), Encoding.UTF8, "application/json"));            
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<Pessoa>(responseString);
+
+            Assert.Equal(data.Nome, pessoaEditada.Nome);
+            Assert.Equal(data.Twitter, pessoaEditada.Twitter);
+        }
+
+        [Fact]
+        public async Task DeveDeletarPessoa()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            await TestDataContext.AddAsync(pessoa);
+            await TestDataContext.SaveChangesAsync();
+
+            var response = await Client.DeleteAsync($"{BaseUrl}/{pessoa.Id}");            
+            response.EnsureSuccessStatusCode();
+
+            response = await Client.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<List<Pessoa>>(responseString);
+
+            Assert.Equal(data.Count, 0);
+        }
+
+        
+        [Fact]
+        public async Task DeveObterPessoa()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            await TestDataContext.AddAsync(pessoa);
+            await TestDataContext.SaveChangesAsync();
+
+            var response = await Client.GetAsync($"{BaseUrl}/{pessoa.Id}");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<Pessoa>(responseString);
+
+            Assert.Equal(data.Id, pessoa.Id);
+            Assert.Equal(data.Nome, pessoa.Nome);
+            Assert.Equal(data.Twitter, pessoa.Twitter);
+        }
+
+        [Fact]
+        public async Task DeveRetornaNaoEncontraoParaAtualizarPessoaComIdInvalido()
+        {          
+           var pessoa = new Pessoa
+           { 
+                Id = 0,
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            var response = await Client.PutAsync($"{BaseUrl}/{pessoa.Id}", new StringContent(JsonConvert.SerializeObject(pessoa), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(response.StatusCode, HttpStatusCode.NotFound);
+        }
+
+         [Fact]
+        public async Task DeveRetornaNaoEncontraoParaDeletarPessoaComIdInvalido()
+        {
+            var pessoa = new Pessoa
+            { 
+                Id = 0,
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            var response = await Client.DeleteAsync($"{BaseUrl}/{pessoa.Id}");
+            
+            Assert.Equal(response.StatusCode, HttpStatusCode.NotFound);
+        }
+
+         [Fact]
+        public async Task DeveRetornaNaoEncontraoParaObterPessoaComIdInvalido()
+        {
+            var pessoa = new Pessoa
+            { 
+                Id = 0,
+                Nome = "Renan Garcia",
+                Twitter = "@_renan_garcia"
+            };
+
+            var response = await Client.GetAsync($"{BaseUrl}/{pessoa.Id}");
+
+            Assert.Equal(response.StatusCode, HttpStatusCode.NotFound);
         }
     }
 }
