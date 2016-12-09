df1178d4c74d3ca5a55f4588a721eef505100650
renan-garcia <email@renangarcia.me>
2016-12-08 22:15:18 -0200

    API complementada

--- src/WebAPIApplication/Controllers/PessoasController.cs
+++ src/WebAPIApplication/Controllers/PessoasController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,11 +18,48 @@ namespace WebAPIApplication {
             return Json(pessoas);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> ObterPessoa(int id){
+            var pessoa = await _dataContext.Pessoas.FindAsync(id);
+
+            if(pessoa == null)
+                return NotFound();
+            
+            return Json(pessoa);
+        }
+
         [HttpPost]
         public async Task<IActionResult> CriaPessoa([FromBody]Pessoa modelo){
             await _dataContext.Pessoas.AddAsync(modelo);
             await _dataContext.SaveChangesAsync();
             return Json(modelo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AtualizaPessoa([FromBody]Pessoa modelo){
+            var pessoa = await _dataContext.Pessoas.FindAsync(modelo.Id);
+
+            if(pessoa == null)
+                return NotFound();
+            
+            pessoa.Nome = modelo.Nome;
+            pessoa.Twitter = modelo.Twitter;
+
+            await _dataContext.SaveChangesAsync();
+            return Json(modelo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RemovePessoa(int id){
+            var pessoa = await _dataContext.Pessoas.FindAsync(id);
+
+            if(pessoa == null)
+                return NotFound();
+            
+            _dataContext.Pessoas.Remove(pessoa);
+            
+            await _dataContext.SaveChangesAsync();
+            return StatusCode((int)HttpStatusCode.OK);
         }
     }
 }
