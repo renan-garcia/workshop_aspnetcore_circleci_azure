0aae6c582750ca45e77796211a6b3139f3e79ef0
renan-garcia <email@renangarcia.me>
2016-12-10 00:16:37 -0200

    Demo Deploy

--- src/WebAPIApplication/Controllers/ValuesController.cs
+++ src/WebAPIApplication/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace WebAPIApplication.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Renan Garcia", "Rafael Cavazza" };
+            return new string[] { "Renan Garcia", "Lealpjm" };
         }
 
         // GET api/values/5
