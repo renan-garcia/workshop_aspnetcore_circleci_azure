e239fcaf4f85960b4fa1c04005bf0bc29ef31158
renan-garcia <email@renangarcia.me>
2017-07-07 15:24:06 -0300

    Teste Kornerz

--- src/WebAPIApplication/Controllers/ValuesController.cs
+++ src/WebAPIApplication/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace WebAPIApplication.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Renan Garcia", "Tokyo" };
+            return new string[] { "Renan Garcia", "Kornerz" };
         }
 
         // GET api/values/5
